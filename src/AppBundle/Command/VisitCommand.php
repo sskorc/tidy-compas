15ac8112bff4c56dc5f3ca3fc3db873c61c8a521
Szymon Skórczyński <szymon.skorczynski@schibsted.pl>
2016-04-01 22:45:07 +0200

    Dummy command

---
+++ src/AppBundle/Command/VisitCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class VisitCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('visit')
+            ->setDescription('Visit URL')
+            ->addArgument(
+                'url',
+                InputArgument::REQUIRED
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $url = $input->getArgument('url');
+
+        $text = 'This is the URL: ' . $url;
+
+        $output->writeln($text);
+    }
+}
