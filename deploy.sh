a731ea1af3718f5c4c8c294b5c1db8dfc017be7a
Szymon Skórczyński <szymon.skorczynski@schibsted.pl>
2016-05-23 21:01:47 +0200

    Added Travis config

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+docker tag sskorc/tidy-compass-dist:latest sskorc/tidy-compass-dist:$TRAVIS_BUILD_NUMBER
+
+docker push sskorc/tidy-compass-dist
+
+curl -u sskorc:$DOCKER_CLOUD_API_KEY -H "Content-Type: application/json" -X POST -d '{"reuse_volumes":false}' https://cloud.docker.com/api/app/v1/service/$PHP_SERVICE_UUID/redeploy/
